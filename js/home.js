c8cd5897f4391ce44e0e7ab328d84dbcf258d1c8
mathieuelisa <mathieu.elisa@gmail.com>
2021-06-25 19:32:05 +0200

    Optimization on JS

--- js/home.js
+++ js/home.js
@@ -1,137 +1,62 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.registerPlugin(CustomEase);
+const scrollBarHome = document.querySelector(".scroll-bar-home");
+const scrollBarAbout = document.querySelector(".scroll-bar-about");
+const scrollBarProjects = document.querySelector(".scroll-bar-projects");
+const scrollBarContact = document.querySelector(".scroll-bar-contact");
 
-let app = {
+const app = {
   buttonScrollTop: document.querySelector(".buttonScroll"),
   buttonScrollTop2: document.querySelector(".topButtonScroll"),
   rootElement: document.documentElement,
   cursor: document.querySelector(".cursor"),
   cursorCenter: document.querySelector(".cursorCenter"),
   textCursor: document.querySelector(".textCursor"),
-  mail: document.querySelector(".myEmail"),
-  toTheTop: document.querySelector(".backToTheTop"),
-  socialLink: document.querySelectorAll(".social-link"),
-  linkProject: document.querySelectorAll(".link-project"),
+  hoverAnims: document.querySelectorAll(".hoverAnim"), // J'ai ajouter la classe 'hoverAnim' à tout tes éléments qui animent ton curseur (dans le HTML)
+  deltaY: 0, // Ici on initialise le delta qui est dans l'event de locomotive
+  locomotiveScroll: null, // On crée une variable qui contiendra l'instance de locomotive pour pouvoir intéragir avec dans le code
 
-  handleTop: () => {
-    document.addEventListener("scroll", () => {
-      let result = app.rootElement.scrollHeight - app.rootElement.clientHeight;
-
-      if (app.rootElement.scrollTop / result > 0.82) {
-        app.buttonScrollTop.classList.add("showBtn");
-        app.buttonScrollTop2.classList.add("showBtn");
-      } else {
-        app.buttonScrollTop.classList.remove("showBtn");
-        app.buttonScrollTop2.classList.remove("showBtn");
-      }
-    });
-  },
   backToTheTop: () => {
     app.buttonScrollTop.addEventListener("click", () => {
-      app.rootElement.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
+      app.locomotiveScroll.scrollTo(0); // Ici on utilise la méthode 'scrollTo' de locomotive
     });
   },
-  changeNameParts: () => {
-    document.addEventListener("scroll", function () {
-      let scrollBarHome = document.querySelector(".scroll-bar-home");
-      let scrollBarAbout = document.querySelector(".scroll-bar-about");
-      let scrollBarProjects = document.querySelector(".scroll-bar-projects");
-      let scrollBarContact = document.querySelector(".scroll-bar-contact");
-
-      let result = app.rootElement.scrollHeight - app.rootElement.clientHeight;
-      let finalResult = app.rootElement.scrollTop / result + 1;
-
-      if (finalResult >= 1 && finalResult <= 1.05) {
-        scrollBarHome.style.display = "block";
-        scrollBarAbout.style.display = "none";
-        scrollBarProjects.style.display = "none";
-        scrollBarContact.style.display = "none";
-      } else if (finalResult >= 1.051 && finalResult <= 1.15) {
-        scrollBarHome.style.display = "none";
-        scrollBarAbout.style.display = "block";
-        scrollBarProjects.style.display = "none";
-        scrollBarContact.style.display = "none";
-      } else if (finalResult >= 1.151 && finalResult <= 1.89) {
-        scrollBarHome.style.display = "none";
-        scrollBarAbout.style.display = "none";
-        scrollBarProjects.style.display = "block";
-        scrollBarContact.style.display = "none";
-      } else if (finalResult >= 1.891) {
-        scrollBarHome.style.display = "none";
-        scrollBarAbout.style.display = "none";
-        scrollBarProjects.style.display = "none";
-        scrollBarContact.style.display = "block";
-      }
-    });
-  },
-  mouseCursor: () => {
-    document.addEventListener("mousemove", (event1) => {
-      let xAxe = event1.pageX;
-      let yAxe = event1.pageY;
-      app.cursor.style.top = yAxe - 15 + "px";
-      app.cursor.style.left = xAxe - 15 + "px";
-    });
-    // document.addEventListener("mousemove", (event2) => {
-    //   let x = event2.pageX;
-    //   let b = event2.pageY;
-    //   app.cursorCenter.style.top = b - 3 + "px";
-    //   app.cursorCenter.style.left = x - 3 + "px";
-    // });
-    document.addEventListener("mousemove", (event3) => {
-      let x = event3.pageX;
-      let y = event3.pageY;
-      app.textCursor.style.top = y - 75 + "px";
-      app.textCursor.style.left = x - 5 + "px";
-    });
-  },
-  animationOnMail: () => {
-    app.mail.addEventListener("mouseenter", () => {
-      app.cursor.classList.add("cursorMail");
-      app.cursorCenter.classList.remove("cursorCenter");
-      app.mail.addEventListener("mouseleave", () => {
-        app.cursor.classList.remove("cursorMail");
-        app.cursorCenter.classList.add("cursorCenter");
-      });
-    });
-  },
-  animationOnButtonToTheTop: () => {
-    app.toTheTop.addEventListener("mouseenter", () => {
-      app.cursor.classList.add("cursorMail");
-      app.cursorCenter.classList.remove("cursorCenter");
-      app.toTheTop.addEventListener("mouseleave", () => {
-        app.cursor.classList.remove("cursorMail");
-        app.cursorCenter.classList.add("cursorCenter");
-      });
-    });
-  },
-  animationOnSocialLink: () => {
-    for (let i = 0; i < app.socialLink.length; i++) {
-      app.socialLink[i].addEventListener("mouseenter", () => {
-        app.cursor.classList.add("cursorMail");
-        app.cursorCenter.classList.remove("cursorCenter");
-        app.socialLink[i].addEventListener("mouseleave", () => {
-          app.cursor.classList.remove("cursorMail");
-          app.cursorCenter.classList.add("cursorCenter");
-        });
+  mouseCursor: (active = true) => {
+    if (active) {
+      document.addEventListener("mousemove", (event) => {
+        const x = event.pageX;
+        const y = event.pageY;
+        // On ajoute le 'deltaY' de locomotive se qui résout le problème de la souris quand on scroll
+        app.cursor.setAttribute(
+          "style",
+          `top: ${y - 15 + app.deltaY}px; left: ${x - 15}px;`
+        );
+        app.cursorCenter.setAttribute(
+          "style",
+          `top: ${y - 3 + app.deltaY}px; left: ${x - 3}px;`
+        );
+        app.textCursor.setAttribute(
+          "style",
+          `top: ${y - 75 + app.deltaY}px; left: ${x - 5}px;`
+        );
       });
     }
   },
-  animationOnProjects: () => {
-    for (let i = 0; i < app.linkProject.length; i++) {
-      app.linkProject[i].addEventListener("mouseenter", () => {
-        app.cursor.classList.add("cursorMail");
-        app.cursorCenter.classList.remove("cursorCenter");
-        app.textCursor.className = "cursorExplore";
-        app.linkProject[i].addEventListener("mouseleave", () => {
-          app.cursor.classList.remove("cursorMail");
-          app.cursorCenter.classList.add("cursorCenter");
+  animMouse: () => {
+    // On ajoute les event de la souris sur tout les éléments qui ont la classe 'hoverAnim'
+    Array.from(app.hoverAnims).forEach((element) => {
+      element.addEventListener("mouseenter", (e) => {
+        app.cursor.classList.toggle("cursorMail");
+        // Ici pour les projets on ajoute une classe particulière
+        if (element.classList.contains("link-project")) {
+          app.textCursor.className = "cursorExplore";
+        }
+      });
+      element.addEventListener("mouseleave", () => {
+        app.cursor.classList.toggle("cursorMail");
+        if (element.classList.contains("link-project")) {
           app.textCursor.className = "textCursor";
-        });
+        }
       });
-    }
+    });
   },
   effectsOnCursor: () => {
     document.addEventListener("click", () => {
@@ -141,150 +66,65 @@ let app = {
         app.cursor.classList.remove("expand");
       }, 500);
     });
+  },
+  onScroll: (event) => {
+    // On met tout le code concernant le scroll dans cette méthode
+    if (event.delta) app.deltaY = event.delta.y;
+    const result = app.rootElement.scrollHeight - app.rootElement.clientHeight;
 
-    window.addEventListener(
-      "scroll",
-      () => {
-        document.body.style.setProperty(
-          "--scroll",
-          window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
-        );
-      },
-      false
+    if (app.rootElement.scrollTop / result > 0.82) {
+      app.buttonScrollTop.classList.add("showBtn");
+      app.buttonScrollTop2.classList.add("showBtn");
+    } else {
+      app.buttonScrollTop.classList.remove("showBtn");
+      app.buttonScrollTop2.classList.remove("showBtn");
+    }
+    const result2 = app.rootElement.scrollHeight - app.rootElement.clientHeight;
+    const finalResult = app.rootElement.scrollTop / result2 + 1;
+
+    // J'ai simplifié ton code ce qui évite de se répéter
+    scrollBarHome.style.display = "none";
+    scrollBarAbout.style.display = "none";
+    scrollBarProjects.style.display = "none";
+    scrollBarContact.style.display = "none";
+    if (finalResult >= 1 && finalResult <= 1.05) {
+      scrollBarHome.style.display = "block";
+    } else if (finalResult >= 1.051 && finalResult <= 1.15) {
+      scrollBarAbout.style.display = "block";
+    } else if (finalResult >= 1.151 && finalResult <= 1.89) {
+      scrollBarProjects.style.display = "block";
+    } else if (finalResult >= 1.891) {
+      scrollBarContact.style.display = "block";
+    }
+    document.body.style.setProperty(
+      "--scroll",
+      window.pageYOffset / (document.body.offsetHeight - window.innerHeight)
     );
   },
-  gsapAnimation: function () {
-    const firstName = document.querySelector(".myName");
-    const mainTitle = document.querySelector(".title-first-container");
-    const mainText = document.querySelector(".text-first-container");
-    const firstBlock = document.querySelector(".bloc-container2-general");
-    // const secondTitle = document.querySelector(".title-second-container");
-    // const secondText = document.querySelector(".mySecondText");
-    // const slider = document.querySelector(".slider");
-    // const myProjects1 = document.querySelector(".first-title-project")
-    // const myProjects1Img = document.querySelector(".mockup-img1")
-    // const myProjects2 = document.querySelector(".second-title-project")
-    // const myProjects2Img = document.querySelector(".mockup-img2")
-    // const myProjects3 = document.querySelector(".third-title-project")
-    // const myProjects3Img = document.querySelector(".mockup-img3")
-    // const myProjects4 = document.querySelector(".fourth-title-project")
-    // const myProjects4Img = document.querySelector(".mockup-img4")
-    // const myProjects5 = document.querySelector(".fifth-title-project")
-    // const myProjects5Img = document.querySelector(".mockup-img5")
-    // const myMail = document.querySelector(".myEmail");
-    // const folio = document.querySelector(".folio");
+  initScroll: () => {
+    // On crée l'instance de locomotive
+    app.locomotiveScroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+      multiplier: 1,
+    });
 
-    CustomEase.create(
-      "myEase",
-      "M0,0 C0,0 -0.003,1.093 0.302,1.048 0.704,0.988 1,1 1,1 "
-    );
-    gsap.fromTo(
-      firstBlock,
-      1.5,
-      { opacity: 0, x: 800 },
-      { opacity: 1, x: 0, ease: "myEase" }
-    );
-
-    gsap.fromTo(
-      firstName,
-      3,
-      { opacity: 0, x: -50 },
-      { opacity: 1, x: 0, delay: 2 }
-    );
-    gsap.fromTo(
-      mainTitle,
-      3,
-      { opacity: 0, x: 50 },
-      { opacity: 1, x: 0, delay: 1.5 }
-    );
-    gsap.fromTo(
-      mainText,
-      3,
-      { opacity: 0, x: 50 },
-      { opacity: 1, x: 0, delay: 1.5 }
-    );
-
-    // gsap.to(secondTitle, 2, { scrollTrigger: secondTitle, y: -10 });
-    // gsap.to(slider, 2, { scrollTrigger: secondTitle, height: 0 });
-
-    // gsap.to(secondText, 2, { scrollTrigger: secondText, y: -20 });
-
-    // gsap.to(myProjects1, 1, {
-    //     scrollTrigger : myProjects1,
-    //     x: 50
-    // })
-    // gsap.to(myProjects1Img, 1, {
-    //     scrollTrigger : myProjects1Img,
-    //     x: -90,
-    // })
-
-    // gsap.to(myProjects2, 1, {
-    //     scrollTrigger: myProjects2,
-    //     x: -50,
-    // })
-    // gsap.to(myProjects2Img, 1, {
-    //     scrollTrigger : myProjects2Img,
-    //     x: 40,
-    // })
-
-    // gsap.to(myProjects3, 1, {
-    //     scrollTrigger : myProjects3,
-    //     x: 50,
-    // })
-    // gsap.to(myProjects3Img, 1, {
-    //     scrollTrigger : myProjects3Img,
-    //     x: -90,
-    // })
-
-    // gsap.to(myProjects4, 1, {
-    //     scrollTrigger : myProjects4,
-    //     x: -50,
-    // })
-    // gsap.to(myProjects4Img, 1, {
-    //     scrollTrigger : myProjects4Img,
-    //     x: 130,
-    // })
-
-    // gsap.to(myProjects5, 1, {
-    //     scrollTrigger : myProjects5,
-    //     x: 50
-    // })
-    // gsap.to(myProjects5Img, 1, {
-    //     scrollTrigger : myProjects5Img,
-    //     x: -90,
-    // })
-
-    // gsap.fromTo(
-    //   myMail,
-    //   3,
-    //   { opacity: 0, scrollTrigger: myMail, y: -60 },
-    //   { opacity: 1, scrollTrigger: myMail, y: 00 }
-    // );
-    // gsap.fromTo(
-    //   folio,
-    //   5,
-    //   { scrollTrigger: folio, opacity: 0 },
-    //   { scrollTrigger: folio, opacity: 1 }
-    // );
+    // On écoute le scroll et on appelle 'app.onScroll'
+    app.locomotiveScroll.on("scroll", app.onScroll);
+    // document.addEventListener('scroll', app.onScroll);
   },
 
   init: function () {
-    app.handleTop(),
-      app.backToTheTop(),
-      app.animationOnButtonToTheTop(),
-      app.mouseCursor(),
-      app.animationOnMail(),
-      app.animationOnSocialLink(),
-      app.animationOnProjects(),
-      app.effectsOnCursor(),
-      app.changeNameParts(),
-      app.gsapAnimation();
+    // Initialisation de scrollBarHome sinon rien est affiché au chargement de la page
+    scrollBarHome.style.display = "block";
+    app.initScroll();
+    app.animMouse();
+    app.backToTheTop();
+    app.mouseCursor();
+    app.effectsOnCursor();
   },
 };
 
 document.addEventListener("DOMContentLoaded", app.init);
 
-const scroll = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
-});
+// J'ai supprimé les autres méthodes ainsi que celle de GSAP
